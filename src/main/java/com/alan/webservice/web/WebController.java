0a4e58e9790c7f00cb069dc349aa1cf315fb8210
Alan <kimym2203@naver.com>
2018-07-28 21:54:35 +0900

    -Test3  ㄴcss,js 추가  ㄴ게시판 리스트 불러오는 db 링크 추가

---
+++ src/main/java/com/alan/webservice/web/WebController.java
@@ -0,0 +1,21 @@
+package com.alan.webservice.web;
+
+import com.alan.webservice.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+    private PostsService postsService;
+
+    @GetMapping("/")
+    public String main(Model model) {
+            model.addAttribute("posts",postsService.findAllDesc());
+            return "main";
+    }
+
+
+}
--- src/main/java/com/alan/webservice/web/WebRestController.java
+++ src/main/java/com/alan/webservice/web/WebRestController.java
@@ -1,7 +1,8 @@
 package com.alan.webservice.web;
 
 import com.alan.webservice.domain.posts.PostsRepository;
-import com.alan.webservice.dto.PostsSaveRequestDto;
+import com.alan.webservice.dto.posts.PostsSaveRequestDto;
+import com.alan.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public  String hello()
@@ -21,9 +22,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
-
-
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
 }
